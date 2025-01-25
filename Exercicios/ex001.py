e9625ac90ff3ac34eed22c31c1e95958bc859f8b
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-25 20:42:37 -0300

    organizando

---
+++ Exercicios/ex001.py
@@ -0,0 +1,11 @@
+#DESAFIO 01d
+nome=input('Qual é seu nome?')
+print('Olá', nome,'! Prazer te conhecer!')
+
+
+
+#DESAFIO 03
+a=int(input('Primeiro número:'))
+b=int(input('Segundo número:'))
+soma=a+b
+print('A soma é', soma)
---
+++ Exercicios/ex002.py
@@ -0,0 +1,5 @@
+#DESAFIO 02
+dia = input('Que dia você nasceu?')
+mes = input('Que mês você nasceu?')
+ano = input('Que ano você nasceu?')
+print("Legal! Você nasceu no dia {} do mês {} de {}!!!".format(dia, mes, ano))
