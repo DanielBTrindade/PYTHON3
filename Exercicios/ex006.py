4fbfd689aff02670f28a6655907129c3ff73d690
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-29 19:13:59 -0300

    ex 007

---
+++ Exercicios/ex006.py
@@ -0,0 +1,6 @@
+#DESAFIO 07
+#CRIE UM ALGORITMO QUE LEIA UM NUMERO E MONTRE O DOBRO O TRIPLO E O QUADRUPLO
+n1 = int(input('Digite um numero: '))
+print('O Dobro de {} é: {}'.format(n1, n1*2))
+print('O Triplo de {} é: {}'.format(n1, n1*3))
+print('O Quadruplo de {} é: {}'.format(n1, n1*4))
