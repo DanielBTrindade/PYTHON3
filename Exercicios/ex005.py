b952429efa2c813d02df7ae24763d8fdd6bc76bc
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-29 18:34:14 -0300

    ex 005

---
+++ Exercicios/ex005.py
@@ -0,0 +1,5 @@
+#DESAFIO 05
+#FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO E MONTRE SEU SUCESSOR E SEU ANTECESSOR.
+
+n1 = int(input('Digite um numero: '))
+print("O antecessor é {}, o sucessor é {}". format(n1 - 1, n1 + 1))
