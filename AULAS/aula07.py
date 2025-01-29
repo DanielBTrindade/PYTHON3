41fa78afb0d68b21bae40524bbd2a8e82c7d77fb
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-29 18:26:03 -0300

    OPERADORES ARITMEDICOS

--- AULAS/aula07.py
+++ AULAS/aula07.py
@@ -1,9 +1,10 @@
+#OPERADORES ARITMÉDICOS
 n1 = int(input('digite um valor: '))
 n2 = int(input('digite outro valor: '))
-print('a soma vale: {}'.format(n1+n2))
+print('a soma vale: {}'.format(n1+n2), end=' ')
 print('a subtração vale: {}'.format(n1-n2))
 print('a multiplicação vale: {}'.format(n1*n2))
-print('a divisão vale: {}'.format(n1/n2))
+print('a divisão vale: {:.3f}'.format(n1/n2))
 print('a potência vale: {}'.format(n1**n2))
 print('a divisão inteira vale: {}'.format(n1//n2))
 print('O resto da divisão vale: {}'.format(n1%n2))
