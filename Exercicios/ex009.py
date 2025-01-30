4e233c92aebc6a3382845f1fdbd717e06a4805ec
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-29 21:29:38 -0300

    ex009

---
+++ Exercicios/ex009.py
@@ -0,0 +1,6 @@
+#DESAFIO 09
+#FAÇA UM PROGRAMA QUE LEIA UM NÚMERO INTEIRO QUALQUER E MOSTRE NA TELA A SUA TABUADA
+#MINHA RESOLUÇÃO
+n = int(input('Qual número você quer sabe a tabuada?'))
+print('A tabuada do {} é:\n{} x 0 = {}\n{} x 1 = {}\n{} x 2 = {}\n{} x 3 = {}\n{} x 4 = {}\n{} x 5 = {}\n{} x 6 = {}\n{} x 7 = {}\n{} x 8 = {}\n{} x 9 = {}\n{} x 10 = {}'.format(n, n, n*0, n, n*1, n, n*2, n, n*3, n, n*4, n, n*5, n, n*6, n, n*7, n, n*8, n, n*9, n, n*10))
+#RESOLUÇÃO DO PROFESSOR
