20fcb5f8b1925b393a681a07925424f4e7adaddc
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-29 21:11:46 -0300

    ex008

---
+++ Exercicios/ex008.py
@@ -0,0 +1,12 @@
+#DESAFIO 08
+#ESCREVA UM PROGRAMA QUE LEIA UM VALOR EM METROS E EXIBA CONVERTIDO EM CENTIMETROS E MILIMETROS
+#MINHA SOLUÇÃO
+n1 = float(input('Quantos metros você quer converter?'))
+print('{} Quilêmetro'.format(n1 / 1000))
+print('{} Hectômetro'.format(n1 / 100))
+print('{} Decâmetro'.format(n1/10))
+print('{} Metros'.format(n1))
+print('{} Decímetro'.format(n1*10))
+print('{} Centímetro'.format(n1*100))
+print('{} Milímetro'.format(n1*1000))
+#SOLUÇÃO DO PROFESSOR
