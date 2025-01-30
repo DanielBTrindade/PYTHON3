394160fc96b5787ce874a7bff5631ea74f96bb2f
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-30 20:20:14 -0300

    ex015

--- Exercicios/ex015.py
+++ Exercicios/ex015.py
@@ -3,11 +3,16 @@
 #E A QUANTIDADE DE DIAS PELOS QUAIS ELE FOI ALUGADO. CALCULE O PREÇO A PAGAR, SABENDO QUE O CARRO CUSTA
 #R$60 POR DIA  E R$0,15 PO KM
 #MINHA SOLUÇÃO
-d = float(input('Por quantos dias o carro foi alugado? '))
+d = int(input('Por quantos dias o carro foi alugado? '))
 km = float(input('Quantos KM foram rodados? '))
 diaria = d * 60
 distancia = km * 0.15
 total = diaria + distancia
-print(f'Total das diárias R${diaria}')
-print(f'Total da distância R${distancia}')
-print(f'Total a pagar é de {total}')
+print(f'Total das diárias R${diaria:.2f}')
+print(f'Total da distância R${distancia:.2f}')
+print(f'Total a pagar é de R${total:.2f}')
+#SOLUÇÃO DO PROFESSOR
+dias = int(input('Quantos dias alugados? '))
+km = float(input('Quantos KM rodados? '))
+pago = (dias * 60) + (km * 0.15)
+print('O total a pagar é de R${:.2f}'.format(pago))
