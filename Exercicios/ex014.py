4a5121ea406a76f48ca970459da24a3ed6d929ae
DANIEL TRINDADE <danielbt751@gmail.com>
2025-01-30 19:57:48 -0300

    ex014

---
+++ Exercicios/ex014.py
@@ -0,0 +1,14 @@
+#DESAFIO 14
+#ESCREVA UM PROGRAMA QUE CONVERTA UMA TEMPERATURA DIGITADA EM ºC PARA ºF
+c = float(input('Qual a temperatura em Cº '))
+f = c * 1.8 + 32
+print(f'A temperatura de {c}ºC equivale a {f}ºF!')
+#DE ºF PARA ºC
+f = float(input('Qual a temperatura em ºF '))
+c = (f - 32) * (5/9)
+print(f'A temperatura de {f}ºF equivale a {c}ºC')
+
+#SOLUÇÃO DO PROFESSOR
+c = float(input('Informe a temperatura em ºC: '))
+f = ((9 * c) / 5) + 32
+print('A temperatura de {}ºC corresponde a {}ºF!'.format(c, f))
