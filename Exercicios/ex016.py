1fd0dae60e0f04a3d978248b75427ae35abfe41d
DANIEL TRINDADE <danielbt751@gmail.com>
2025-02-12 20:12:33 -0300

    Update ex016.py

--- Exercicios/ex016.py
+++ Exercicios/ex016.py
@@ -2,5 +2,5 @@
 # E MOSTRE NA TELA SUA PARTE INTEIRA
 from math import trunc
 num = float(input('Digite um número real qualquer: '))
-inteiro = trunc(num)
-print(f'A porção inteira de {num} é igual a {inteiro}')
+print(f'A porção inteira de {num} é igual a {trunc(num)}')
+print(f'A porção inteira de {num} é igual a {int(num)}')
