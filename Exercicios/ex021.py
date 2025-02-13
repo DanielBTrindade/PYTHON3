94f5fa1e7b24e7d0e1b20456f685efb80751fda5
DANIEL TRINDADE <danielbt751@gmail.com>
2025-02-13 19:07:22 -0300

    aula 09

--- Exercicios/ex021.py
+++ Exercicios/ex021.py
@@ -3,12 +3,13 @@ import pygame
 #inicilizar o mixer
 pygame.mixer.init()
 #carregar o arquivo de música(verificar o caminho)
-pygame.mixer.music.load('../../../../hungria.mp3')
+pygame.mixer.music.load('ex021.mp3')
 #tocar a música
 pygame.mixer.music.play()
 #se quiser encerrar
 input('Pressione ENTER para terminar')
 pygame.mixer.music.stop()
+
 
 
 
