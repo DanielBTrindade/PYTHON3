16fa0f88f988653399f8603bd47f1a062302a4c3
DANIEL TRINDADE <danielbt751@gmail.com>
2025-02-27 20:45:45 -0300

    update

--- AULAS/aula11.py
+++ AULAS/aula11.py
@@ -3,9 +3,19 @@ print('\33[4;30;45mOlá mundo\033[m')
 print('\33[1;36;47mOlá mundo\033[m')
 print('\33[30;41mOlá mundo\033[m')
 nome = 'DANIEL'
-cores = {'limpa':'\033[m',
-         'azul':'\033[34m',
-         'amarelo':'\033[33m',
-         'pretoebranco':'\033[7;30m'}
-print(f'Olá! Muito prazer em te conhecer,{cores["pretoebranco"]} {nome} {cores["limpa"]}')
+cores = {
+    'limpa': '\033[m',
+    'vermelho': '\033[31m',
+    'verde': '\033[32m',
+    'amarelo': '\033[33m',
+    'azul': '\033[34m',
+    'magenta': '\033[35m',
+    'ciano': '\033[36m',
+    'branco': '\033[37m',
+    'pretoebranco': '\033[7;30m',
+    'negrito': '\033[1m',
+    'sublinhado': '\033[4m'
+}
+# Exemplo de uso
+print(f'Olá! Muito prazer em te conhecer, {cores["verde"]}{nome}{cores["limpa"]}')
 
