c3b11b4bbdeb09c66c96a2d27c6e78a720297657
DANIEL TRINDADE <danielbt751@gmail.com>
2025-02-27 13:20:45 -0300

    ex031

--- Exercicios/ex031.py
+++ Exercicios/ex031.py
@@ -2,14 +2,15 @@
 #cobrando R$0,50 por km para viagens até 200 km e R$0,45 para viagens mais longas
 #MINHA SOLUÇÃO
 #Receber a distância da viagem
+from time import sleep
 distancia = float(input('Qual a distância em KM da sua viagem? '))
 #calcular o valor da passagem da viagem até 200 km
+print('CALCULANDO...')
+sleep(2)
 if distancia <= 200:
-    d1 = distancia * 0.50
-    # mostrar o valor da passagem da viagem até 200 km
-    print(f'Sua passagem vai custar {d1:.2f}')
+    preco = distancia * 0.50
 # calcular o valor da passagem da viagem mais longa
 else:
-    d2 = distancia * 0.45
-    # mostrar o valor da passagem da viagem mais longa
-    print(f'Sua passagem vai custar {d2:.2f}')
+    preco = distancia * 0.45
+# mostrar o valor da passagem
+print(f'Sua passagem vai custar {preco:.2f}')
