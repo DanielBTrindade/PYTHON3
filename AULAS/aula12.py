a2ac6102f0c26ecafc3754d936d47e6732323eec
DANIEL TRINDADE <danielbt751@gmail.com>
2025-06-03 08:46:40 -0300

    update 01

--- AULAS/aula12.py
+++ AULAS/aula12.py
@@ -1,1 +1,7 @@
-some = str(input("Qual é o seu nome?"))
+nome = str(input("Qual é o seu nome?"))
+if nome in ['Daniel', 'Gabrieli']:
+    print(f'{nome} é um nome bonito!')
+elif nome in ['Pedro', 'Maria', 'João']:
+    print("Seu nome é bem popular no Brasil!")
+else:
+    print("Seu nome é bem normal!")
