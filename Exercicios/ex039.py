c27f40fada80b52f4a181e5ce38c8bff57354e4b
DANIEL TRINDADE <danielbt751@gmail.com>
2025-06-04 17:01:04 -0300

    up01

--- Exercicios/ex039.py
+++ Exercicios/ex039.py
@@ -8,12 +8,13 @@ ano = int(input('DIGITE sua data de nascimento: '))
 ano_atual = datetime.now().year
 idade = ano_atual - ano
 ano_alistamento = ano + 18
+falta = 18 - idade
 if idade < 18:
-    print(f'Ainda não chegou o ano do seu alistamento. \nVocê deve se alistar em {ano_alistamento}.')
+    print(f'Ainda não chegou o ano do seu alistamento. \nVocê deve se alistar em {ano_alistamento}.\nFalta {falta} anos para seu alistamento.')
 elif idade == 18:
     print('PARABÉNS, ESSE É O ANO DO SEU ALISTAMENTO.')
 elif idade > 18:
-    print(f'Já passou o ano do seu alistamento.\nDeveria ter se alistado em {ano_alistamento}.')
+    print(f'Já passou o ano do seu alistamento.\nDeveria ter se alistado em {ano_alistamento}.\nJa passaram {falta} anos do seu alistamento.')
 else:
     print('INFORMAÇÃO INVÁLIDA.\nTENTE NOVAMENTE.')
 
