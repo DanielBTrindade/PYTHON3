1823d97cb3002bea060ee75f4f299ddf9e3f55b4
DANIEL TRINDADE <danielbt751@gmail.com>
2025-06-04 17:07:33 -0300

    apdate

--- Exercicios/ex039.py
+++ Exercicios/ex039.py
@@ -4,19 +4,23 @@
 #- SE JA PASSOU A HORA DE SE ALISTAR
 #O PROGRAMA TAMBÉM DEVE MOSTRAR O TEMPO QUE FALTA OU QUE PASSOU DO PRAZO.
 from datetime import datetime
-ano = int(input('DIGITE sua data de nascimento: '))
-ano_atual = datetime.now().year
-idade = ano_atual - ano
-ano_alistamento = ano + 18
-falta = 18 - idade
-if idade < 18:
-    print(f'Ainda não chegou o ano do seu alistamento. \nVocê deve se alistar em {ano_alistamento}.\nFalta {falta} anos para seu alistamento.')
-elif idade == 18:
-    print('PARABÉNS, ESSE É O ANO DO SEU ALISTAMENTO.')
-elif idade > 18:
-    print(f'Já passou o ano do seu alistamento.\nDeveria ter se alistado em {ano_alistamento}.\nJa passaram {falta} anos do seu alistamento.')
-else:
-    print('INFORMAÇÃO INVÁLIDA.\nTENTE NOVAMENTE.')
+try:
+    ano = int(input('DIGITE sua data de nascimento: '))
+    ano_atual = datetime.now().year
+    idade = ano_atual - ano
+    ano_alistamento = ano + 18
+    if idade < 18:
+        falta = 18 - idade
+        print(f'Ainda não chegou o ano do seu alistamento. \nVocê deve se alistar em {ano_alistamento}.'
+              f'\nFalta {falta} anos para seu alistamento.')
+    elif idade == 18:
+        print('PARABÉNS, ESSE É O ANO DO SEU ALISTAMENTO.')
+    elif idade > 18:
+        passou = idade - 18
+        print(f'Já passou o ano do seu alistamento.\nDeveria ter se alistado em {ano_alistamento}.'
+              f'\nJa passaram {passou} anos do seu alistamento.')
+except ValueError:
+    print("Erro! Certifique-se de digitar um número válido para o ano de nascimento.")
 
 
 
