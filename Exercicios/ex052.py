efbeea223a68bbf1433faf9ce123206831f61d00
DANIEL TRINDADE <danielbt751@gmail.com>
2025-06-08 18:13:33 -0300

    numeros primos

--- Exercicios/ex052.py
+++ Exercicios/ex052.py
@@ -1,13 +1,14 @@
 # FAÇA UM PROGRAMA QUE LEIA UM NUMERO INTEIRO E DIGA SE ELA É OU NÃO UM NUMERO PRIMO
+import math
 n = int(input("Verificar números primos ate: "))
-multiplos = 0
+mult = 0
 
-for count in range(2,n):
-    if (n % count == 0):
-            print("Múltiplo de",count)
-        multiplos += 1
+for c in range(2,n): #testa os múltiplos. de N no intervalo entre 2 e N
+    if n % c == 0:
+        print("Múltiplo de",c)
+        mult += 1
 
-if(multiplos==0):
+if mult == 0:
     print("É primo")
 else:
-    print("Tem",multiplos," múltiplos acima de 2 e abaixo de",n)
+    print("Tem",mult," múltiplos acima de 2 e abaixo de",n)
