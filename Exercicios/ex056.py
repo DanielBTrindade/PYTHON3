544f3d4e2d4ba917b98748d875793954b061bf30
DANIEL TRINDADE <danielbt751@gmail.com>
2025-06-08 21:01:25 -0300

    ex056

--- Exercicios/ex056.py
+++ Exercicios/ex056.py
@@ -2,3 +2,15 @@
 # A MEDIA DE IDADE DO GRUPO
 # NOME DO HOMEM MAIS VELHO
 # QUANTAS MULHERES TEM MENOS DE 20 ANOS.
+media = 0
+velho = 0
+for c in range(0, 2):
+    nome = str(input('Digite seu nome: '))
+    idade = int(input('Digite sua idade: '))
+    sexo = int(input('Sexo: [1]Masculino [2]Feminino'))
+    media += idade / 2
+    velho = nome
+
+print(f'O homem mais velho é {velho}, {idade} anos.')
+print(f'media de idade {media}')
+
